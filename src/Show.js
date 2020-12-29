9ab56ce40513597ee3dadf893caa9e90393018cc
js4484 <js4484@columbia.edu>
2020-12-29 15:31:05 -0500

    Show skeleton

--- src/Show.js
+++ src/Show.js
@@ -1,13 +1,73 @@
-import { withRouter } from 'react-router-dom';
 import React from 'react';
-import { Link } from 'react-router-dom';
+
 
 class Show extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      input: ""
+      input: "",
+      data: props.data,
+      output: this.constructResult(this.state.data)
     }
+
+  }
+
+  constructResult(data) {
+    let picture = <div className="plant-pic"><a href="data.picture"></a></div>;
+    let nameDouple = <div className="name-douple">
+                        <div className="name-common">data.commmonname</div>
+                        <div className="name-sci">data.sciencename</div>
+                    </div>;
+    let light = <div className="result"> 
+          <div className="result-name">Light</div>
+          <div className="result-range">
+            <div className="result-name">Shade</div>
+            <div className="result-no">-</div>
+            <div className="result-no">-</div>
+            <div className="result-yes">l</div>
+            <div className="result-yes">l</div>
+            <div className="result-no">-</div>
+            <div className="result-name">Full Sun</div>
+          </div>
+        </div>;
+    let soil = 
+        <div className="result">
+          <div className="result-name">Soil Water Retention</div>
+          <div className="result-range">
+            <div className="result-name">Sand</div>
+            <div className="result-yes">l</div>
+            <div className="result-yes">l</div>
+            <div className="result-no">-</div>
+            <div className="result-no">-</div>
+            <div className="result-no">-</div>
+            <div className="result-name">Mud</div>
+          </div>
+        </div>;
+      
+    let humidity = <div className="result">
+          <div className="result-name">Humidity</div>
+          <div className="result-range">
+            <div className="result-name">Desert</div>
+            <div className="result-yes">l</div>
+            <div className="result-yes">l</div>
+            <div className="result-no">-</div>
+            <div className="result-no">-</div>
+            <div className="result-no">-</div>
+            <div className="result-name">Swamp</div>
+          </div>
+        </div>;
+    
+    
+    return (
+      <div className="result-plant">
+          {picture}
+          {nameDouple}
+          {light}  
+          
+      </div>
+      );
+
+    
   }
 
   handleSearch(e) {
@@ -28,54 +88,8 @@ class Show extends React.Component {
       <div className="MainApp">
 
 
-        <div className="result-plant">
-          <div className="plant-pic"><a href="https://i.pinimg.com/originals/38/2a/ad/382aad040d3a8de15aab8f3e62e2b21a.jpg"></a></div>
-          <div className="name-douple">
-            <div className="name-common">Fishbone Cactus</div>
-            <div className="name-sci">(Epiphyllum anguliger)</div>
-          </div>
-        </div>
-
-        <div className="result"> 
-          <div className="result-name">Light</div>
-          <div className="result-range">
-            <div className="result-name">Shade</div>
-            <div className="result-no">-</div>
-            <div className="result-no">-</div>
-            <div className="result-yes">l</div>
-            <div className="result-yes">l</div>
-            <div className="result-no">-</div>
-            <div className="result-name">Full Sun</div>
-          </div>
-        </div>
-         
-        <div className="result">
-          <div className="result-name">Soil Water Retention</div>
-          <div className="result-range">
-            <div className="result-name">Sand</div>
-            <div className="result-yes">l</div>
-            <div className="result-yes">l</div>
-            <div className="result-no">-</div>
-            <div className="result-no">-</div>
-            <div className="result-no">-</div>
-            <div className="result-name">Mud</div>
-          </div>
-        </div>
-
-        <div className="result">
-          <div className="result-name">Humidity</div>
-          <div className="result-range">
-            <div className="result-name">Desert</div>
-            <div className="result-yes">l</div>
-            <div className="result-yes">l</div>
-            <div className="result-no">-</div>
-            <div className="result-no">-</div>
-            <div className="result-no">-</div>
-            <div className="result-name">Swamp</div>
-          </div>
-        </div>
-          
-          
+        {this.state.output}
+ 
           
       </div>
     );
